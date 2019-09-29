8b1930ad90d2bc2597a3ab55fba56afa7547998c
Han Wang <hyw2@cornell.edu>
2019-09-29 16:06:26 -0400

    Add course search function

--- requirements.js
+++ requirements.js
@@ -1,22 +1,51 @@
 // Check README.md file for information of Roster API
 
-// Get latest acadGroups from API (FA19)
-$.get("https://classes.cornell.edu/api/2.0/config/acadGroups.json?roster=FA19", data => {
+// Get latest acadGroups from API
+$.get("https://classes.cornell.edu/api/2.0/config/acadGroups.json", {roster: 'FA19'}, data => {
     const colleges = data.data.acadGroups;
     // Loop through all objects in colleges
     colleges.map(college => {
-        $("#colleges").append(`<option value=${college.value}>${college.descr}</option>`)
+        $("#colleges").append(`<option value=${college.value}>${college.descr}</option>`);
     })
 });
 
-// Get latest subjects from API (FA19)
-$.get("https://classes.cornell.edu/api/2.0/config/subjects.json?roster=FA19", data => {
+// Get latest subjects from API
+$.get("https://classes.cornell.edu/api/2.0/config/subjects.json", {roster: 'FA19'}, data => {
     const subjects = data.data.subjects;
     // Loop through all objects in subjects
     subjects.map(subject => {
-        $("#subjects").append(`<option value=${subject.value}>${subject.descr}</option>`)
+        $("#subjects").append(`<option value=${subject.value}>${subject.descr}</option>`);
     })
 });
+
+// Search latest roster for class
+function search() {
+
+    // Get text from search input
+    const input = $("#search").val();
+
+    // Split name into subject (sub) and number (num)
+    const sub = input.split(" ")[0].toUpperCase();
+    const num = input.split(" ")[1];
+
+
+    $.get("https://classes.cornell.edu/api/2.0/search/classes.json", { roster: 'FA19', subject: sub, q: num }, data => {
+        
+        console.log(data);
+        if (data.status === "success") {
+
+            const course = data.data.classes[0];
+
+            // Append dismissible alert with course info
+            $("#courses").append(`<div class="alert alert-secondary alert-dismissible fade show" role="alert">
+            ${course.subject} ${course.catalogNbr}: ${course.titleLong}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>`);
+        }
+    });
+}
 
 function requirements() {
     const reqs = $("#reqs");
