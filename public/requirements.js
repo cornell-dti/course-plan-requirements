3b01a8eefcd90bd6feb6ad1bbe9162b5194c13e0
Sam <sam@developersam.com>
2019-10-23 14:21:33 -0400

    Setup CI and fix existing linter errors
    Tried the preserve the style as much as I can.
    Temporarily make some linter errors as warnings to unblock CI deployment. These problems should be fixed later rather than ignored.

--- public/requirements.js
+++ public/requirements.js
@@ -1,43 +1,43 @@
 // Check README.md file for information of Roster API
 // Libraries used: jQuery, Bootstrap
 
+/* global $ */
+
 // Get latest acadGroups from API
-$.get("https://classes.cornell.edu/api/2.0/config/acadGroups.json", {roster: 'FA19'}, data => {
+$.get('https://classes.cornell.edu/api/2.0/config/acadGroups.json', { roster: 'FA19' }, (data) => {
     const colleges = data.data.acadGroups;
     // Loop through all objects in colleges
-    colleges.map(college => {
-        $("#colleges").append(`<option value=${college.value}>${college.descr}</option>`);
-    })
+    colleges.forEach((college) => {
+        $('#colleges').append(`<option value=${college.value}>${college.descr}</option>`);
+    });
 });
 
 // Get latest subjects from API
-$.get("https://classes.cornell.edu/api/2.0/config/subjects.json", {roster: 'FA19'}, data => {
-    const subjects = data.data.subjects;
+$.get('https://classes.cornell.edu/api/2.0/config/subjects.json', { roster: 'FA19' }, (data) => {
+    const { subjects } = data.data;
     // Loop through all objects in subjects
-    subjects.map(subject => {
-        $("#subjects").append(`<option value=${subject.value}>${subject.descr}</option>`);
-    })
+    subjects.forEach((subject) => {
+        $('#subjects').append(`<option value=${subject.value}>${subject.descr}</option>`);
+    });
 });
 
 // Search latest roster for class
+// eslint-disable-next-line no-unused-vars
 function search() {
-
     // Get text from search input
-    const input = $("#search").val();
+    const input = $('#search').val();
 
     // Split name into subject (sub) and number (num)
-    const sub = input.split(" ")[0].toUpperCase();
-    const num = input.split(" ")[1];
+    const sub = input.split(' ')[0].toUpperCase();
+    const num = input.split(' ')[1];
 
-    $.get("https://classes.cornell.edu/api/2.0/search/classes.json", { roster: 'FA19', subject: sub, q: num }, data => {
-        
+    $.get('https://classes.cornell.edu/api/2.0/search/classes.json', { roster: 'FA19', subject: sub, q: num }, (data) => {
         console.log(data);
-        if (data.status === "success") {
-
+        if (data.status === 'success') {
             const course = data.data.classes[0];
 
             // Append dismissible alert with course info
-            $("#courses").append(`<div class="alert alert-secondary alert-dismissible fade show" role="alert">
+            $('#courses').append(`<div class="alert alert-secondary alert-dismissible fade show" role="alert">
             ${course.subject} ${course.catalogNbr}: ${course.titleLong}
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
@@ -47,12 +47,13 @@ function search() {
     });
 }
 
+// eslint-disable-next-line no-unused-vars
 function requirements() {
     // Define requirement section
-    const reqs = $("#reqs");
+    const reqs = $('#reqs');
 
     // Clear all text inside element
     reqs.empty();
     // Add html to element
-    reqs.append(`<p>Total Credits: 120<p>`);
+    reqs.append('<p>Total Credits: 120<p>');
 }
--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-const password = require('./password').password;
 // Import express for creating REST APIs
 const express = require('express');
+
 const app = express();
 
 // Import MongoDB client package
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
+const { password } = require('./password');
+
 const uri = `mongodb+srv://admin:${password}@course-plan-t2nrj.mongodb.net/test?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Load all files in /public folder
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
-app.get("/college", (req, res) => {
+app.get('/college', (req, res) => {
+    const { name } = req.query;
 
-    let name = req.query.name;
-
-    client.connect(err => {
-        const collection = client.db("course-plan").collection("requirements");
-        collection.find().toArray((err, doc) => {
+    client.connect((err) => {
+        const collection = client.db('course-plan').collection('requirements');
+        collection.find().toArray((error, doc) => {
             res.send(doc);
             client.close();
         });
-    })
-})
+    });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log("Listening on port "+PORT);
+    console.log(`Listening on port ${PORT}`);
 });
